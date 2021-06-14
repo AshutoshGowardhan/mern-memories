2936b5843b3c14c32bb748c2da1381b83ba3e61f
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-06-14 22:18:59 +0530

    css styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 import Form from './components/Form/Form';
-import Posts from './components/Posts/Posts'
-import memories from './images/memories.jpg'
+import Posts from './components/Posts/Posts';
+import memories from './images/memories.jpg';
+import useStyles from './styles'
 const App = () => {
+    const classes = useStyles();
+
     return (
-        <Container maxidth='lg'>
-            <AppBar position="static" color="inherit">
-                <Typography variant='h2' align="center">Memories</Typography>
-                <img src={memories} alt="memories" height="60" />
+        <Container maxWidth='lg'>
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant='h2' align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="icon" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grow conatiner justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
                             <Posts />
                         </Grid>
-                        <Grid item xs={12} sm={7}>
+                        <Grid item xs={12} sm={4}>
                             <Form />
                         </Grid>
 
-                    </Grow>
+                    </Grid>
                 </Container>
             </Grow>
         </Container>
