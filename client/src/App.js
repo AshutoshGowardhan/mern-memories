dcd2c20b61c37f63799dd15879a48143870cc309
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-06-14 23:22:48 +0530

    actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 import Form from './components/Form/Form';
 import Posts from './components/Posts/Posts';
 import memories from './images/memories.jpg';
 import useStyles from './styles'
 import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts'
 const App = () => {
     const classes = useStyles();
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [])
 
     return (
         <Container maxWidth='lg'>
